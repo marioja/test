b1a871feaa889ae990644899e5668b5bd286b013
mario <mfj@earthling.net>
2018-02-23 16:23:04 -0500

    initial commit

---
+++ net.mfjassociates.ui.markers/src/net/mfjassociates/ui/markers/handlers/ExportContribution.java
@@ -0,0 +1,19 @@
+package net.mfjassociates.ui.markers.handlers;
+
+import org.eclipse.ui.menus.ExtensionContributionFactory;
+import org.eclipse.ui.menus.IContributionRoot;
+import org.eclipse.ui.services.IServiceLocator;
+
+public class ExportContribution extends ExtensionContributionFactory {
+
+	public ExportContribution() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void createContributionItems(IServiceLocator serviceLocator, IContributionRoot additions) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
