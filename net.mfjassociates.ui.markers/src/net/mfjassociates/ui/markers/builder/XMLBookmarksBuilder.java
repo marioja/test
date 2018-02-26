79d23dcb3a11d3605c95a75a4342b44f3c274df3
Mario Jauvin <mfj@earthling.net>
2018-02-25 22:29:01 -0500

    Added support to create bookmarks in XML file or write bookmakrs.xml

--- net.mfjassociates.ui.markers/src/net/mfjassociates/ui/markers/builder/XMLBookmarksBuilder.java
+++ net.mfjassociates.ui.markers/src/net/mfjassociates/ui/markers/builder/XMLBookmarksBuilder.java
@@ -1,7 +1,16 @@
 package net.mfjassociates.ui.markers.builder;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.text.MessageFormat;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.IntStream;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
@@ -16,6 +25,11 @@ import org.eclipse.core.resources.IResourceVisitor;
 import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.Path;
+import org.w3c.dom.DOMException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
 import org.xml.sax.helpers.DefaultHandler;
@@ -82,7 +96,20 @@ public class XMLBookmarksBuilder extends IncrementalProjectBuilder {
 
 	private static final String MARKER_TYPE = "net.mfjassociates.ui.markers.xmlProblem";
 
+	private static final String XML_FILE_TYPE = ".xml";
+
+	private static final String BOOKMAKRS_XML_FILE_TYPE = ".bookmarks.xml";
+
 	private SAXParserFactory parserFactory;
+	
+	private static DocumentBuilder docBuilder=null;
+	static {
+		try {
+			docBuilder=DocumentBuilderFactory.newInstance().newDocumentBuilder();
+		} catch (ParserConfigurationException e) {
+		}
+
+	}
 
 	private void addMarker(IFile file, String message, int lineNumber,
 			int severity) {
@@ -120,20 +147,103 @@ public class XMLBookmarksBuilder extends IncrementalProjectBuilder {
 	}
 
 	void checkXML(IResource resource) {
-		if (resource instanceof IFile && resource.getName().endsWith(".xml")) {
+		if (resource instanceof IFile) {
 			IFile file = (IFile) resource;
+			if (file.getName().endsWith(XML_FILE_TYPE) && !file.getName().endsWith(BOOKMAKRS_XML_FILE_TYPE)) {
+				IFile bookmarksfile=bookmarksFile(file);
+				long xmltime=file.getLocalTimeStamp();
+				long bookmarkstime=bookmarksfile.getLocalTimeStamp();
+				if (xmltime>bookmarkstime || bookmarksfile.getModificationStamp()==IResource.NULL_STAMP) { // xml file was changed, rewrite the bookmarks
+					System.out.println("XML file updated, rewriting bookmarks file");
+					exportMarkers(file, bookmarksfile, xmltime-1);
+				}
+				
+			}
+			if (file.getName().endsWith(BOOKMAKRS_XML_FILE_TYPE)) {
+				IFile xmlfile=xmlFile(file);
+				long xmltime=xmlfile.getLocalTimeStamp();
+				long bookmarkstime=file.getLocalTimeStamp();
+				if (bookmarkstime>xmltime && !(file.getModificationStamp()==IResource.NULL_STAMP)) { // bookmarks xml file was changed, update bookmarks
+					deleteMarkers(xmlfile, IMarker.BOOKMARK);
+					try {
+						Document doc = docBuilder.parse(file.getContents());
+						NodeList markers = doc.getElementsByTagName("marker");
+						Bookmark bookmark=new Bookmark(xmlfile);
+						IntStream.range(0, markers.getLength()).mapToObj(markers::item).map(bookmark::createMarker).count();
+						
+					} catch (SAXException | IOException | CoreException e) {
+						e.printStackTrace();
+					}
+					System.out.println("Bookmarks file updated, updating bookmarks");
+				}
+			}
+		}
+		/*if (resource instanceof IFile && resource.getName().endsWith(".xml") && !resource.getName().endsWith("bookmarks.xml")) {
+			IFile file = (IFile) resource;
+			exportMarkers(file);
 			deleteMarkers(file);
 			XMLErrorHandler reporter = new XMLErrorHandler(file);
 			try {
 				getParser().parse(file.getContents(), reporter);
 			} catch (Exception e1) {
 			}
+		}*/
+	}
+	class Bookmark {
+		private IFile file;
+		public Bookmark(IFile aFile) {
+			this.file = aFile;
+		}
+		private IMarker createMarker(Node node) {
+			IMarker retmarker=null;
+			try {
+				final IMarker marker = file.createMarker(IMarker.BOOKMARK);
+				retmarker = marker;
+				NodeList attributes=node.getChildNodes();
+				IntStream.range(0,  attributes.getLength()).mapToObj(attributes::item).forEach(attribute -> fillMarker(marker, attribute));
+			} catch (CoreException e) {
+			}
+			return retmarker;
+		}
+		private void fillMarker(final IMarker marker, Node attribute) {
+			try {
+				if (attribute.getNodeType()==Node.ELEMENT_NODE) {
+					Object value=null;
+					String attributeName=attribute.getNodeName();
+					if (!"message".equals(attributeName)) {
+						value=new Integer(attribute.getTextContent());
+					} else {
+						try {
+							value=attribute.getTextContent();
+						} catch (NumberFormatException e) {
+						}
+					}
+					if (value!=null) marker.setAttribute(attributeName, value);
+				}
+			} catch (DOMException | CoreException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
-	private void deleteMarkers(IFile file) {
+	private IFile xmlFile(IFile file) {
+		String fn=file.getName();
+		int l=fn.length();
+		String fileName=fn.substring(0, l-BOOKMAKRS_XML_FILE_TYPE.length())+XML_FILE_TYPE;
+		return file.getParent().getFile(new Path("/"+fileName));
+	}
+
+	private IFile bookmarksFile(IFile file) {
+		String fn=file.getName();
+		int l=fn.length();
+		String ft="."+file.getFileExtension();
+		String bookmarksFileName=fn.substring(0, l-ft.length())+BOOKMAKRS_XML_FILE_TYPE;
+		return file.getParent().getFile(new Path("/"+bookmarksFileName));
+	}
+
+	private void deleteMarkers(IFile file, String markerType) {
 		try {
-			file.deleteMarkers(MARKER_TYPE, false, IResource.DEPTH_ZERO);
+			file.deleteMarkers(markerType, false, IResource.DEPTH_ZERO);
 		} catch (CoreException ce) {
 		}
 	}
@@ -158,5 +268,36 @@ public class XMLBookmarksBuilder extends IncrementalProjectBuilder {
 			IProgressMonitor monitor) throws CoreException {
 		// the visitor does the work.
 		delta.accept(new XMLBookmarksDeltaVisitor());
+	}
+	private void exportMarkers(IFile file, IFile bookmarksfile, long previousStamp) {
+		IMarker[] bookmarks =null;
+		ByteArrayOutputStream baos=new ByteArrayOutputStream();
+		PrintStream bookmarksfileps=new PrintStream(baos);
+		int depth = IResource.DEPTH_INFINITE;
+		try {
+			bookmarksfileps.println("<markers>");
+			bookmarks = file.findMarkers(IMarker.BOOKMARK, true, depth);
+			for (IMarker marker : bookmarks) {
+				bookmarksfileps.println("  <marker>");
+				Map<String, Object> attrs = marker.getAttributes();
+				Set<String> ks = attrs.keySet();
+				for (String key : ks) {
+					bookmarksfileps.println(MessageFormat.format("    <{0}>{1}</{0}>", key, attrs.get(key)));
+				}
+				bookmarksfileps.println("  </marker>");
+			}
+			bookmarksfileps.println("</markers>");
+			bookmarksfileps.close();
+			ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+			if (bookmarksfile.exists()) bookmarksfile.setContents(bais, true, false, null);
+			else bookmarksfile.create(bais, true, null);
+		} catch (CoreException e) {
+			// ignore
+		} finally {
+			try {
+				bookmarksfile.setLocalTimeStamp(previousStamp);
+			} catch (CoreException e) {
+			}
+		}
 	}
 }
